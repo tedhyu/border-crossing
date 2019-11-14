d55fe0992a5ee58b18eb3519bf6ead206c09b524
tedhyu <49349498+tedhyu@users.noreply.github.com>
2019-11-14 15:26:18 -0800

    Update border_analytics.py

--- src/border_analytics.py
+++ src/border_analytics.py
@@ -40,6 +40,7 @@ with open(exportname, 'w') as out_f:
     out_f.write('Border,Date,Measure,Value,Average\n')
     for l in list3:
         l=l[0]+','+l[1].strftime('%m/%d/%Y %I:%M:%S %p')+','+l[2]+','+str(l[3])+','+str(l[4])+'\n'
+        #change date back to original format
         out_f.write(l)
         
 
